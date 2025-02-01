32629e6c62d55e4171ac223fa34ddea3f0ea2a79
ajclarkson <adam@ajclarkson.co.uk>
2025-02-01 17:40:46 +0000

    Re-add netlify configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,16 @@ import { defineConfig } from 'astro/config';
 import { remarkReadingTime } from './remark-reading-time.mjs';
 import preact from "@astrojs/preact";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://ajclarkson.co.uk",
   integrations: [preact()],
+
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
 
+  adapter: netlify(),
 });
